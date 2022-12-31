80d8192c44aaa9e29ca530e5649113dabe5c5067
leechulhan <dhfhfkwhdk@naver.com>
2022-12-31 16:02:43 +0900

    SQL Practice Commit

--- SQL-exercise/SQL_exercise_08/8_questions_answer_byLee.sql
+++ SQL-exercise/SQL_exercise_08/8_questions_answer_byLee.sql
@@ -136,18 +136,118 @@ SELECT * FROM Undergoes; -- 수슬하는 의사 List
  	   		 WHERE u.stay = s.stayid 
  	   ) 
  	   
- 	   
--- TODO 여기서부터 다시 시작 하기 !!!!! (blocoCode 및 Block Floor 숙지 !!!) 	   
 -- 8.7 Obtain the names of all the nurses who have ever been on call for room 123.
 SELECT * FROM nurse n;
 SELECT * FROM on_call oc;
 SELECT * FROM block b;  
-SELECT * FROM 
+SELECT * FROM room r;
+		SELECT * 
+		  FROM nurse n
+	     WHERE n.employeeID IN (
+	     	SELECT oc.nurse
+	     	  FROM on_call oc
+	    -- INNER JOIN block b 
+	   	--	ON b.Blockfloor = oc.BlockFloor AND b.BlockCode = oc.blockcode
+	   	INNER JOIN room r 
+	   			ON r.blockFloor = oc.BlockFloor AND r.BlockCode = oc.BlockCode 
+	     --  WHERE oc.nurse = n.EmployeeID ---> 정답이랑 비교 시 위 에 IN 에 넣어주기 때문에 굳이 필요 없는듯 ... ???
+	     	 WHERE r.RoomNumber = '123'
+	     );	  
  	  
- 	  
- 	  -- 8.8 The hospital has several examination rooms where appointments take place. Obtain the number of appointments that have taken place in each examination room.
+-- 8.8 The hospital has several examination rooms where appointments take place. Obtain the number of appointments that have taken place in each examination room.
+	SELECT * FROM room r;
+    SELECT * FROM appointment a;
+	   SELECT 
+	   			count(*)
+	   		,	a.ExaminationRoom AS '방번호'	
+	     FROM appointment a
+	 GROUP BY a.ExaminationRoom;
+	    
 -- 8.9 Obtain the names of all patients (also include, for each patient, the name of the patient's primary care physician), such that \emph{all} the following are true:
-    -- The patient has been prescribed some medication by his/her primary care physician.
-    -- The patient has undergone a procedure with a cost larger that $5,000
-    -- The patient has had at least two appointment where the nurse who prepped the appointment was a registered nurse.
+-- The patient has been prescribed some medication by his/her primary care physician.
+-- The patient has undergone a procedure with a cost larger that $5,000
+-- The patient has had at least two appointment where the nurse who prepped the appointment was a registered nurse.
+-- The patient's primary care physician is not the head of any department.
+	
+	-- names of All patient with primary care physician
+SELECT * FROM patient;	
+SELECT * FROM physician;
+SELECT * FROM medication m;
+SELECT * FROM procedures p;
+SELECT * FROM prescribes p;
+	  
+	  SELECT * 
+	    FROM patient p
+  INNER JOIN physician ph	
+  		  ON p.pcp = ph.employeeid;
+	   
+  	-- Patient All Name List	 
+  	   SELECT *
+	     FROM patient p
+	    WHERE p.pcp = (
+	    	SELECT p2.employeeid 
+	    	  FROM physician p2
+ 	    	 WHERE p.pcp = p2.employeeid
+	    );   
+	   
+	   
+	-- patient has been prescribed some medication by his/her primary care physician.
+	SELECT * FROM prescribes p2;
+	SELECT * FROM physician;
+	SELECT * FROM patient;
+	SELECT p3.Medication
+	     , m.name 
+	  FROM patient p
+INNER JOIN physician p2	  
+		ON p.pcp = p2.employeeid
+INNER JOIN prescribes p3 
+        ON p.ssn = p3.patient
+INNER JOIN medication m 
+        ON p3.medication = m.code;
+		
+    -- The patient has undergone a procedure with a cost larger that $5,000   
+    SELECT * FROM prescribes p2;
+	SELECT * FROM physician;
+	SELECT * FROM patient;
+	SELECT * FROM procedures p;   
+	SELECT * FROM undergoes u;  
+	SELECT *
+	  FROM patient p 
+INNER JOIN undergoes u  
+        ON p.ssn = u.patient   
+     WHERE u.procedures IN (
+     	SELECT p2.code 
+     	  FROM procedures p2 
+         WHERE p2.cost > 5000
+     );     
+	
+    -- TODO 문제 이해가 안됨 다시 확인 하여보기 !!!!!
+	-- The patient has had at least two appointment where the nurse who prepped the appointment was a registered nurse.
+	SELECT * FROM patient p;
+	SELECT * FROM nurse n ;	
+	SELECT * FROM appointment a;
+	SELECT *
+	  FROM patient p
+     WHERE p.ssn IN (
+     	SELECT * 
+     	  FROM appointment a  
+     	 WHERE p.ssn = a.patient 
+     );
+	
+    -- TODO 문제 이해가 안됨 다시 확인 하여보기 !!!!!
     -- The patient's primary care physician is not the head of any department.
+    SELECT * FROM patient p;
+	SELECT * FROM physician p2;	
+	SELECT * FROM Department;
+
+	SELECT *
+	  FROM patient p 
+     WHERE p.pcp != (
+     	SELECT p2.employeeid
+     	  FROM physician p2
+    INNER JOIN department d
+     	    ON p2.employeeid = d.head 
+     	 WHERE p.pcp = p2.employeeid 
+     );
+
+    
